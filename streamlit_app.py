65e7270d7c51409ab7e1996d87ec84041bdf2f0b
m-b-m <57490534+m-b-m@users.noreply.github.com>
2025-04-21 23:35:54 +0000

    extra uitleg.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,7 @@ st.write("Upload een CSV met geotags (GPSLatitude, GPSLongitude in DMS-formaat),
 2. Ga in de terminal naar de map waar je foto's staan. Voor de Apple Photo's app is dat bijvoorbeeld zoiets als `~/Pictures/Photos Library.photoslibrary/originals`
 3. Voer dit commando uit: `exiftool -csv -gpslatitude -gpslongitude -datetimeoriginal -c "%d %d %.8f" -fast2 -r . > ~/Desktop/gps-data.csv`
 4. Upload dit bestand hier.
+5. Op https://www.google.com/mymaps kan je een mooi kaartje maken met de .csv
 """
 
 # Uploadbestand
